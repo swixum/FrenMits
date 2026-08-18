ba63e00e10d5b6848202fec9afe84f0f193fbf12
swix <>
2026-08-18 03:10:46 -0400

    fixing issues

--- FrenAlerts/FrenAlerts.Engine/EngineInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace FrenAlerts.Engine;
-
-// The status command prints this, which is what proves the engine assembly
-// actually shipped in the zip and loaded, rather than failing at the first call.
-public static class EngineInfo
-{
-    public static string Version =>
-        typeof(EngineInfo).Assembly.GetName().Version?.ToString() ?? "unknown";
-}
